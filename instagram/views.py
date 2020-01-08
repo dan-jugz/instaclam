486ed59446e58d64e991f58dbe222e8433fe7ae0
daniel njuguna <njugunadaniel364@gmail.com>
2020-01-08 05:57:26 +0300

    write a function allowing user to signup appropriately

--- instagram/views.py
+++ instagram/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .forms import SignUpForm, UpdateUserForm, UpdateUserProfileForm, PostForm, CommentForm
+from django.contrib.auth import login, authenticate
 from .models import Post, Comment, Profile, Follow
 from django.contrib.auth.models import User
 
 # Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
+    
 def index(request):
     images = Post.objects.all()
     params = {
