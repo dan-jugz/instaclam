6e715996d2cdbfee27d21b16372a08f60bb19023
daniel njuguna <njugunadaniel364@gmail.com>
2020-01-08 05:29:19 +0300

    define a function to render the homepage

--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Post, Comment, Profile, Follow
+from django.contrib.auth.models import User
 
 # Create your views here.
+def index(request):
+    images = Post.objects.all()
+    params = {
+        'images': images,
+    }
+    return render(request, 'instagram/index.html', params)   
