3879721afa044ec2f3f21c3c96d20623d8ee5260
daniel njuguna <njugunadaniel364@gmail.com>
2020-01-08 05:15:21 +0300

    create a profile model to save the user

--- instagram/models.py
+++ instagram/models.py
@@ -5,6 +5,18 @@ from django.dispatch import receiver
 
 # Create your models here.
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    profile_picture = models.ImageField(upload_to='images/', default='default.png')
+    bio = models.TextField(max_length=500, default="My Bio", blank=True)
+    name = models.CharField(blank=True, max_length=120)
+    location = models.CharField(max_length=60, blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+
+        
 class Post(models.Model):
     image = models.ImageField(upload_to='posts/')
     name = models.CharField(max_length=250, blank=True)
