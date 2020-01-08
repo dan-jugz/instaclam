5ca1cb8f180ea6e78f091766495087b18e4c7d3b
daniel njuguna <njugunadaniel364@gmail.com>
2020-01-08 06:30:45 +0300

    set a route for  the account and profile pages

--- instagram/urls.py
+++ instagram/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
+    path('account/', include('django.contrib.auth.urls')),
     path('', views.index, name='index'),
+    path('profile/<username>/', views.profile, name='profile'),
+    path('user_profile/<username>/', views.user_profile, name='user_profile'),
 ]
